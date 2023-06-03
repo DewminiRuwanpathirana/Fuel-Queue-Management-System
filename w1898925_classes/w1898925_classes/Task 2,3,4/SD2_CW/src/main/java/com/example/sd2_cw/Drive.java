db7d384148c78ef43f8bf050ce7a928da473e447
DewminiRuwanpathirana <108386047+DewminiRuwanpathirana@users.noreply.github.com>
2023-06-04 01:58:39 +0530

    Delete w1898925_classes/w1898925_classes/Task 2,3,4/SD2_CW directory

--- w1898925_classes/w1898925_classes/Task 2,3,4/SD2_CW/src/main/java/com/example/sd2_cw/Drive.java
+++
@@ -1,7 +0,0 @@
-package com.example.sd2_cw;
-
-public class Drive extends passenger {
-    public static void main(String[] args) {
-        mainMethod();
-    }
-}
--- w1898925_classes/w1898925_classes/Task 2,3,4/SD2_CW/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sd2_cw {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sd2_cw to javafx.fxml;
-    exports com.example.sd2_cw;
-}
